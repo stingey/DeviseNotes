e39d8724e87684fe284554807f6f2a7aa4b46d62
Spenser <spenser.tingey@simplefinance.com>
2017-01-19 14:21:24 -0700

    made form for adding notes

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -4,5 +4,25 @@ class NotesController < ApplicationController
     @user = current_user
     @notes = Note.where(user_id: current_user)
   end
+  def new
+    @user = current_user
+    @note = current_user.notes.build
+  end
 
+  def create
+    @note = current_user.notes.build(note_params)
+    if @note.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+  def show
+    @note = Note.find(params[:id])
+  end
+
+  private
+  def note_params
+    params.require(:note).permit(:title, :body)
+  end
 end

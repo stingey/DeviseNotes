95deca449557a53dc4c93d280a52768a167393c8
Spenser <spenser.tingey@simplefinance.com>
2017-01-19 09:18:58 -0700

    added notes model

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,8 @@
 class NotesController < ApplicationController
   before_action :authenticate_user!
   def index
-
+    @user = current_user
+    @notes = Note.where(user_id: current_user)
   end
 
 end

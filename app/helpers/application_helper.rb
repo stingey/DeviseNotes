fed656963c9230edbe73afda5df34b5ac5d7db2f
Spenser <spenser.tingey@simplefinance.com>
2017-01-25 14:37:49 -0700

    added styling for signup pages and title for tab

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def page_title(title = '')
+    base_title = 'Note Tracker'
+    if title.empty?
+      base_title
+    elsif user_signed_in?
+      title + " | " + current_user.first_name
+    else
+      title + " | " + base_title
+    end
+
+  end
 end

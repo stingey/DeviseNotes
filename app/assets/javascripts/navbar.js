5da924b76fa6a5eb7c605224c9b794b0f8db3c41
Spenser <spenser.tingey@simplefinance.com>
2017-02-14 12:23:39 -0700

    spruced up the navbar a little more

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,8 +2,12 @@ $(document).on("scroll", function() {
     if ($(document).scrollTop() > 25) {
        $("nav").addClass('small');
        $(".logo").addClass('small');
+       $(".taz").addClass('small');
+       $('.drop-down').finish().hide(0);
     } else {
        $("nav").removeClass('small');
        $(".logo").removeClass('small');
+       $(".taz").removeClass('small');
+       $('.drop-down').fadeIn();
     }
   }.bind(this));

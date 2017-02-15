1c5578b8aedbd2f06cdd2c103d6099acf9287922
Spenser <spenser.tingey@simplefinance.com>
2017-02-15 11:35:05 -0700

    minor touch ups

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -2,12 +2,10 @@ $(document).on("scroll", function() {
     if ($(document).scrollTop() > 25) {
        $("nav").addClass('small');
        $(".logo").addClass('small');
-       $(".taz").addClass('small');
        $('.drop-down').finish().hide(0);
     } else {
        $("nav").removeClass('small');
        $(".logo").removeClass('small');
-       $(".taz").removeClass('small');
-       $('.drop-down').fadeIn();
+       $('.drop-down').delay(50).fadeIn();
     }
   }.bind(this));
